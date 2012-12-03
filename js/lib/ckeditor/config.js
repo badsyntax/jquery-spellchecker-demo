b64890da393a42d42fe41361ec72d1960fa6646a
Richard Willis <willis.rh@gmail.com>
2012-12-03 21:29:19 +0000

    Updated CKEditor to 4.0 stable

--- js/lib/ckeditor/config.js
+++ js/lib/ckeditor/config.js
@@ -1,94 +1,35 @@
-﻿/**
- * Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-  // Define changes to default configuration here. For example:
-  // config.language = 'fr';
-  // config.uiColor = '#AADC6E';
-  config.plugins =
-    'jqueryspellchecker,' +
-    'basicstyles,' +
-    'preview,' +
-    'removeformat,' +
-    'resize,' +
-    'save,' +
-    'sourcearea,' +
-    'toolbar,' +
-    'htmlwriter,' +
-    'preview,' +
-    'print,' +
-    'wysiwygarea';
-};  
-
 /**
  * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-// CKEDITOR.editorConfig = function( config ) {
-//   // Define changes to default configuration here. For example:
-//   // config.language = 'fr';
-//   // config.uiColor = '#AADC6E';
-//   // %REMOVE_START%
-//   config.plugins =
-//     'jqueryspellchecker,' +
-//     'about,' +
-//     'a11yhelp,' +
-//     'basicstyles,' +
-//     'bidi,' +
-//     'blockquote,' +
-//     'clipboard,' +
-//     'colorbutton,' +
-//     'colordialog,' +
-//     'contextmenu,' +
-//     'div,' +
-//     'elementspath,' +
-//     'enterkey,' +
-//     'entities,' +
-//     'find,' +
-//     'flash,' +
-//     'floatingspace,' +
-//     'font,' +
-//     'format,' +
-//     'forms,' +
-//     'horizontalrule,' +
-//     'htmlwriter,' +
-//     'image,' +
-//     'iframe,' +
-//     'indent,' +
-//     'justify,' +
-//     'link,' +
-//     'list,' +
-//     'magicline,' +
-//     'maximize,' +
-//     'newpage,' +
-//     'pagebreak,' +
-//     'pastefromword,' +
-//     'pastetext,' +
-//     'preview,' +
-//     'print,' +
-//     'removeformat,' +
-//     'resize,' +
-//     'save,' +
-//     'scayt,' +
-//     'selectall,' +
-//     'showblocks,' +
-//     'showborders,' +
-//     'smiley,' +
-//     'sourcearea,' +
-//     'specialchar,' +
-//     'stylescombo,' +
-//     'tab,' +
-//     'table,' +
-//     'tabletools,' +
-//     'templates,' +
-//     'toolbar,' +
-//     'undo,' +
-//     'wsc,' +
-//     'wysiwygarea';
-//   // %REMOVE_END%
-// };
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For the complete reference:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-// // %LEAVE_UNMINIFIED% %REMOVE_LINE%
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+
+	// Remove some buttons, provided by the standard plugins, which we don't
+	// need to have in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Extra plugins
+	config.extraPlugins = 'jqueryspellchecker';
+};
